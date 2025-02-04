c18cb03e5ff23bd14784823b89a3087f712d1f60
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-02-04 12:36:39 +0330

    Nice

---
+++ app/env/index.js
@@ -0,0 +1,14 @@
+// ----------------------------------------------
+// $app/env
+// index.js
+// ----------------------------------------------
+// Env.
+// Just using a place to export our env.
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const env = process.env;
+
+export default env;
